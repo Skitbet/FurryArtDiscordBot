1f1dabaf2d243d3eee949530922e3b2de5b42118
Skitbet <54096334+Skitbet@users.noreply.github.com>
2023-05-27 03:32:03 -0400

    Update Tokens.java

--- src/main/java/com/skitbet/zephyr/utils/Tokens.java
+++ src/main/java/com/skitbet/zephyr/utils/Tokens.java
@@ -1,6 +1,6 @@
 package com.skitbet.zephyr.utils;
 
 public class Tokens {
-    public static String TOKEN="MTEwMzU0MDYzMTY2MzU0MjMzMw.GinUBx.eaHi1mImG2QFF19UO6uUK7VQxoiVi6f0xHSEt4";
-    public static String CLIENT_ID="1103540631663542333";
+    public static String TOKEN="MTEwMzU0MDYzMTY2MzU0MjMzMw.GinUBx.eaHi1mImG2QFF19UO6uUK7VQxoiVi6f0xHSEt4"; // Example Token
+    public static String CLIENT_ID="1103540631663542333"; // Example ID
 }
